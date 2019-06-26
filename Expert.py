58f485bda01527ed8ef110234a5bd1f656bce448
sopi10 <raphael@southpigalle.io>
2019-06-26 09:58:53 +0200

    replace for in sys.argv by len(argv)

--- Expert.py
+++ Expert.py
@@ -112,9 +112,7 @@ class Parsing:
         """
         First fonction called
         """
-        index = 0
-        for elem in sys.argv:
-            index += 1
+        index = len(sys.argv)
         if index != 2:
             raise OSError("Need one argument")
         self.filename = sys.argv[1]
